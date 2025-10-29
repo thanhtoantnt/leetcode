96b5f8cd2cd1a33a2ee3da3059b4d020bbd34a57
thanhtoantnt <thanhtoantnt@gmail.com>
2025-10-29 23:24:32 +0800

    updates

--- two_pointers/11_medium.py
+++ two_pointers/11_medium.py
@@ -9,3 +9,28 @@ class Solution:
 
         
         return volumn
+
+from typing import List
+
+class SolutionOpt:
+    def maxArea(self, heights: List[int]) -> int:
+        left = 0
+        right = len(heights) - 1
+        max_volume = 0
+        
+        while left < right:
+            # Calculate current volume
+            height = min(heights[left], heights[right])
+            width = right - left
+            current_volume = height * width
+            
+            # Update maximum
+            max_volume = max(max_volume, current_volume)
+            
+            # Move the pointer with the smaller height
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_volume
